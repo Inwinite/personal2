4b77af6d58a365354f212badb2bdd23c1f9a9e84
Manuel Seguez Ocantos <manuelseguez@gmail.com>
2020-09-21 00:39:04 -0300

    cambiando de todo, sacando joomla y horde

--- horde/ingo/config/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* CONFIG START. DO NOT CHANGE ANYTHING IN OR AFTER THIS LINE. */
-// $Id: 48142d13ef06c07f56427fe5b43981631bdbfdb0 $
-$conf['storage']['params']['driverconfig'] = 'horde';
-$conf['storage']['driver'] = 'sql';
-$conf['rules']['userheader'] = false;
-$conf['spam']['compare'] = 'string';
-/* CONFIG END. DO NOT CHANGE ANYTHING IN OR BEFORE THIS LINE. */
--- horde/turba/config/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* CONFIG START. DO NOT CHANGE ANYTHING IN OR AFTER THIS LINE. */
-// $Id: 4cd616848fb2e5c81200bf7c65930e9086ec2dcd $
-$conf['menu']['import_export'] = false;
-$conf['comments']['allow'] = false;
-$conf['documents']['type'] = 'horde';
-$conf['tags']['enabled'] = false;
-/* CONFIG END. DO NOT CHANGE ANYTHING IN OR BEFORE THIS LINE. */
